f149ecc46b1cc333b11cb0ca387105dde573324a
= <rfremmer@hotmail.com>
2025-01-02 18:21:59 -0800

    initial commit

---
+++ WebApplicationHello5/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationHello5.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
